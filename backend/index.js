3844e2f96173cfc5d3211375c4895334f620b257
uzuw <uzuwxd@gmail.com>
2025-04-19 23:43:44 +0545

    index.js setup

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const {graphqlHTTP}=require('express-graphql');
+const cors=require('cors')
+const schema=require('./schema/schema')
+const colors=require('colors')
+
+//importing the enviroment varibles
+require('dotenv').config();
+const PORT=process.env.PORT || 5000;
+
+//express app instance
+const app=express();
+
+//connecting to the database
+//..
+
+//cors setup
+app.use(cors());
+
+//graphql endpoint
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql:process.env.NODE_ENV==='development'
+}))
+
+
+
+app.listen(PORT,console.log(`Server is now running in the port ${PORT}`));
