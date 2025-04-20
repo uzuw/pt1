3810da9b8c44474c46806c405b68443c77719407
uzuw <uzuwxd@gmail.com>
2025-04-20 23:06:01 +0545

    connecting to db

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const {graphqlHTTP}=require('express-graphql');
 const cors=require('cors')
 const schema=require('./schema/schema')
 const colors=require('colors')
+const connectDB=require('./config/db')
 
 //importing the enviroment varibles
 require('dotenv').config();
@@ -12,16 +13,16 @@ const PORT=process.env.PORT || 5000;
 const app=express();
 
 //connecting to the database
-//..
+connectDB();
 
 //cors setup
 app.use(cors());
 
 //graphql endpoint
-app.use('/graphql',graphqlHTTP({
-    schema,
-    graphiql:process.env.NODE_ENV==='development'
-}))
+// app.use('/graphql',graphqlHTTP({
+//     schema,
+//     graphiql:process.env.NODE_ENV==='development'
+// }))
 
 
 
