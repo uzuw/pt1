2633abe0f0e231a5cbcf621d1c2dec701dd0ae3f
uzuw <uzuwxd@gmail.com>
2025-04-22 18:35:10 +0545

    generate jsonwebtoken function

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,13 @@
+const jwt=require('jsonwebtoken');
+
+const generateToken=(user)=>{
+    return jwt.sign({
+        id:user._id,
+        email:user.email
+    },
+    process.env.JWT_SECRET,
+    {expiresIn:'7d'}
+    );
+};
+
+module.exports=generateToken;
