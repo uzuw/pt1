556f72f60eab368818d0d31e26bb09e8cb30e488
uzuw <uzuwxd@gmail.com>
2025-04-23 18:35:01 +0545

    update model

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -1,3 +1,4 @@
+const { urlencoded } = require('express');
 const mongoose=require('mongoose');
 
 const ProjectSchema= new mongoose.Schema({
@@ -18,7 +19,31 @@ const ProjectSchema= new mongoose.Schema({
     startDate:{
         type:Date,
         default:Date.now //setting current date when the db model is created 
-    }},
+    },
+    githubRepoUrl: {
+        type: String,
+        validate: {
+          validator: function(v) {
+            return /^(https?:\/\/)?(www\.)?github\.com\/[A-Za-z0-9_.-]+\/[A-Za-z0-9_.-]+$/.test(v);
+          },
+          message: props => `${props.value} is not a valid GitHub repo URL!`
+        }
+      },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    tags:{
+        type:[String],
+        enum:['Backend','Frontend','Devops','Other'],
+        default:undefined
+    },
+    completedDate: {
+        type: Date,
+        default: null
+      }
+    },
     {
         timestamps:true //to add the createdAt and udatedAt
     }
